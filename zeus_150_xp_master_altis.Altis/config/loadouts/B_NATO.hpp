4a07bb3d5452035da424c6977ef0d52a26e66c11
wagneriguess <33234782+wagneriguess@users.noreply.github.com>
2017-11-01 12:28:02 -0400

    Fixed Medic and added binocs to every class

--- zeus_150_xp_master_altis.Altis/config/loadouts/B_NATO.hpp
+++ zeus_150_xp_master_altis.Altis/config/loadouts/B_NATO.hpp
@@ -105,13 +105,13 @@ class loadouts
 		backpackClass = "B_Kitbag_rgr";
 		
 		linkedItems[] = {"ItemMap", "ItemCompass", "ItemWatch", "TFAR_anprc152", "ItemMicroDAGR", "NVGoggles_INDEP"}; // Linked items for the unit, use for map, compass, radio, watch, gps, and NVGs
-		uniformItems[] = {{"30Rnd_9x21_Mag", 2}, {"ACE_EntrenchingTool", 1}, {"ItemcTabHCam", 1}}; // Items to place in uniform. Includes weapon magazines
-		vestItems[] = {{"30Rnd_65x39_caseless_mag", 5}, {"30Rnd_65x39_caseless_mag_Tracer", 2}}; // Items to place in vest. Includes weapon magazines
+		uniformItems[] = {{"ItemcTabHCam", 1}}; // Items to place in uniform. Includes weapon magazines
+		vestItems[] = {{"30Rnd_65x39_caseless_mag", 5}, {"30Rnd_65x39_caseless_mag_Tracer", 2}, {"ACE_EntrenchingTool", 1}, {"30Rnd_9x21_Mag", 2}}; // Items to place in vest. Includes weapon magazines
 		backpackItems[] = {{"SmokeShellGreen", 4}, {"SmokeShell", 6}}; // Items to place in backpack. Includes weapon magazines
 		
-		basicMedUniform[] = {}; // Items to be placed in the uniform only when basic medical is being used
-		basicMedVest[] = {{"ACE_fieldDressing", 40}, {"ACE_morphine", 15}, {"ACE_epinephrine", 15}}; // Items to be placed in the vest only when basic medical is being used
-		basicMedBackpack[] = {{"ACE_bloodIV", 6}, {"ACE_bloodIV_500", 10}, {"ACE_morphine", 15}, {"ACE_epinephrine", 15}, {"ACE_fieldDressing", 20}}; // Items to be placed in the backpack only when basic medical is being used
+		basicMedUniform[] = {{"ACE_fieldDressing", 10}, {"ACE_morphine", 5}, {"ACE_epinephrine", 5}}; // Items to be placed in the uniform only when basic medical is being used
+		basicMedVest[] = {}; // Items to be placed in the vest only when basic medical is being used
+		basicMedBackpack[] = {{"ACE_bloodIV", 6}, {"ACE_bloodIV_500", 10}, {"ACE_morphine", 25}, {"ACE_epinephrine", 25}, {"ACE_fieldDressing", 50}}; // Items to be placed in the backpack only when basic medical is being used
 		
 		advMedUniform[] = {}; // Items to be placed in the uniform only when advanced medical is being used
 		advMedVest[] = {}; // Items to be placed in the vest only when advanced medical is being used
